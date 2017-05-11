7817163b1a61e8c20eb52fed738dcde6ec5de2e5
Samarth Agarwal <samarthagarwal@live.com>
2017-05-11 14:25:08 +0530

    Added Update for REST API v2

--- services.js
+++ services.js
@@ -2,11 +2,13 @@ angular.module('starter.services',[])
 .service('WC', function(){
     return {
         WC: function(){
-            var Woocommerce = new WooCommerceAPI.WooCommerceAPI({
-                url: 'http[s]://Your-Site-Url.com',
+            var Woocommerce = new WoocommerceAPI({
+                url: 'http://www.your_website.com',
                 consumerKey: 'ck_your_consumer_key_here',
-                consumerSecret: 'cs_your_consumer_secret_here'
-            });
+                consumerSecret: 'cs_your_consumer_secret_here',
+				wpAPI: true, //or false if you want to use the legacy API v3
+  				version: 'wc/v2' //or wc/v1
+            })
             return Woocommerce;
         }
 }});
