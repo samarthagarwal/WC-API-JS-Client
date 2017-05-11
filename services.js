f03261b4db851862fc5a0b01b45b3a4ed8750aa3
Samarth Agarwal <samarthagarwal@live.com>
2017-05-11 14:30:13 +0530

    Update services.js

--- services.js
+++ services.js
@@ -6,8 +6,8 @@ angular.module('starter.services',[])
                 url: 'http://www.your_website.com',
                 consumerKey: 'ck_your_consumer_key_here',
                 consumerSecret: 'cs_your_consumer_secret_here',
-				wpAPI: true, //or false if you want to use the legacy API v3
-  				version: 'wc/v2' //or wc/v1
+		wpAPI: true, //or false if you want to use the legacy API v3
+  		version: 'wc/v2' //or wc/v1
             })
             return Woocommerce;
         }
